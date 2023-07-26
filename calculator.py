cd848ff59c58711377e1a2787329068c6fc0cecd
digvijay1807 <115910570+digvijay1807@users.noreply.github.com>
2023-07-26 22:28:31 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,13 @@
+#Calculator
+
+a=int(input("Enter first number:-"))
+b=int(input("Enter second number:-"))
+print("Addition is",a+b)
+print("Subtraction is",a-b)
+print("Multiplication is",a*b)
+print("Division is",a/b)
+print("Floor value is",a//b)
+print("Value of a raise to b is",a**b)
+print("Remainder is",a%b)
+
+
